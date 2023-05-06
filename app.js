d4325e00e02df72a3b305ea7943e26308e867b47
通晓宇宙 <2581011320@qq.com>
2023-05-06 22:51:20 +0800

    3

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/web/index');
+//导入 account 接口路由文件
+const accountRouter = require('./routes/api/account');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', accountRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
