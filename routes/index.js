6c56d2b487a1f1381e0980ce044d2144a2dc0de6
通晓宇宙 <2581011320@qq.com>
2023-05-06 22:46:59 +0800

    mongodb

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,28 @@ const adapter = new FileSync(__dirname + '/../data/db.json');
 const db = low(adapter);
 //导入 shortid
 const shortid = require('shortid');
+//导入 moment
+const moment = require('moment');
+const AccountModel = require('../models/AccountModel');
+
+//测试
+// console.log(moment('2023-02-24').toDate())
+//格式化日期对象
+// console.log(moment(new Date()).format('YYYY-MM-DD'));
 
 //记账本的列表
 router.get('/account', function(req, res, next) {
   //获取所有的账单信息
-  let accounts = db.get('accounts').value();
-  res.render('list', {accounts: accounts});
+  // let accounts = db.get('accounts').value();
+  //读取集合信息
+  AccountModel.find().sort({time: -1}).exec((err, data) => {
+    if(err){
+      res.status(500).send('读取失败~~~');
+      return;
+    }
+    //响应成功的提示
+    res.render('list', {accounts: data, moment: moment});
+  })
 });
 
 //添加记录
@@ -23,12 +39,19 @@ router.get('/account/create', function(req, res, next) {
 
 //新增记录
 router.post('/account', (req, res) => {
-  //生成 id
-  let id = shortid.generate();
-  //写入文件
-  db.get('accounts').unshift({id:id, ...req.body}).write();
-  //成功提醒
-  res.render('success', {msg: '添加成功哦~~~', url: '/account'});
+  //插入数据库
+  AccountModel.create({
+    ...req.body,
+    //修改 time 属性的值
+    time: moment(req.body.time).toDate()
+  }, (err, data) => {
+    if(err){
+      res.status(500).send('插入失败~~');
+      return
+    }
+    //成功提醒
+    res.render('success', {msg: '添加成功哦~~~', url: '/account'});
+  })
 });
 
 //删除记录
@@ -36,9 +59,14 @@ router.get('/account/:id', (req, res) => {
   //获取 params 的 id 参数
   let id = req.params.id;
   //删除
-  db.get('accounts').remove({id:id}).write();
-  //提醒
-  res.render('success', {msg: '删除成功~~~', url: '/account'});
+  AccountModel.deleteOne({_id: id}, (err, data) => {
+    if(err) {
+      res.status(500).send('删除失败~');
+      return;
+    }
+    //提醒
+    res.render('success', {msg: '删除成功~~~', url: '/account'});
+  })
 });
 
 module.exports = router;
